86844cf99dd9d5815a021e6aa8e9028fe27f480b
Shabnam <shabnam.ofl@gmail.com>
2026-06-01 12:18:16 +0530

    feat: add traffic light mac-os style window controls

--- main.py
+++ main.py
@@ -569,7 +569,7 @@ if __name__ == "__main__":
                 min_size=(520, 760),
                 resizable=False,
                 fullscreen=False,
-                frameless=False,
+                frameless=True,
                 background_color="#0d1117",
             )
             webview.start()
